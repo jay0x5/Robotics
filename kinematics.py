a143981825a6f894a71dd0a0630811983a224451
Jay <78173760+jay0x5@users.noreply.github.com>
2022-09-29 18:00:31 +0530

    Update kinematics.py

--- kinematics.py
+++ kinematics.py
@@ -2,9 +2,9 @@ import math
 
 
 #in mm (Values from CAD model)
-thighLength = 100 # Side B
+thighLength = 100 # Side B [#thigh a.k.a Femur]
 HipToGroundlength = 190 #Side C
-FemurLength = 100 #Side A
+TibiaLength = 100 #Side A [#Tibia a.k.a Shin]
 
 
 
@@ -13,15 +13,16 @@ FemurLength = 100 #Side A
 
 
 #get Angle A
+
 # cos A = sqaure(b) + sqaure(c) - sqaure(a)
-hipangleA = pow(thighLength,2) + pow(HipToGroundlength,2) - pow(FemurLength,2)
+hipangleA = pow(thighLength,2) + pow(HipToGroundlength,2) - pow(TibiaLength,2)
 
 #2 * b * c
 hipangleA2 = 2 * thighLength * HipToGroundlength
 
 
 hipangleAfinal = hipangleA /hipangleA2
-acosfunc = math.acos(hipangleAfinal)
+acosfunc = math.acos(hipangleAfinal) #inverse cos function in radians
 CosAinDeg = math.degrees(acosfunc) #converts rad to deg
 print(CosAinDeg)
 
@@ -29,16 +30,16 @@ print(CosAinDeg)
 
 # get Angle B
 # cos B = sqaure(c) + sqaure(a) - sqaure(b)
-hipangleB = pow(HipToGroundlength,2) + pow(FemurLength,2) - pow(thighLength,2)
+hipangleB = pow(HipToGroundlength,2) + pow(TibiaLength,2) - pow(thighLength,2)
 
 #2 * c * a
-hipangleB2 = 2 * HipToGroundlength * FemurLength
+hipangleB2 = 2 * HipToGroundlength * TibiaLength
 
 
 hipangleBfinal = hipangleB /hipangleB2
 acosfuncB = math.acos(hipangleBfinal)
 CosBinDeg = math.degrees(acosfuncB) #converts rad to deg
-print(CosBinDeg)
+# print(CosBinDeg) Angle B = A since Side B = A
 
 
 
@@ -46,6 +47,9 @@ print(CosBinDeg)
 AngleC = 180 - CosAinDeg - CosBinDeg 
 print(AngleC)
 
+#subtracting an offset of 45 and multiplying by the scaling factor
+finalC = (AngleC - 45) * 25
+print(finalC)
 
 
 
